b168a2bdc1238e51c92d3e368e011014d30b72fa
NikitaSparkle <71117913+nikitasparkle@users.noreply.github.com>
2020-11-28 15:45:06 +0200

    finished

--- src/main/java/org/itstep/List.java
+++ src/main/java/org/itstep/List.java
@@ -2,13 +2,13 @@ package org.itstep;
 
 import java.util.Arrays;
 
-// FIXME: обобщенный тип должен быть классом расширяющим интерфейс Comparable<T>
-class List<T> {
+// FIXME: обобщенный тип должен быть классом расширяющим интерфейс Comparable<T> +
+class List<T extends Comparable<T>>{
     private T[] arrays;
 
     public List() {
-        // FIXME: создать массив типов Comparable
-        this.arrays = (T[]) new Object[0];
+        // FIXME: создать массив типов Comparable +
+        this.arrays = (T[]) new Comparable[0];
     }
 
     public void add(T item) {
@@ -22,23 +22,37 @@ class List<T> {
 
     public T min() {
         T min = arrays[0];
-        // FIXME: реализовать поиск наименьшего элемента используя метод compareTo интерфейса IComparable<T>
+        // FIXME: реализовать поиск наименьшего элемента используя метод compareTo интерфейса IComparable<T> +
         /*for(T item: arrays) {
             if(item < min) {
                 min = item;
             }
         }*/
+        for (int i = 0; i < arrays.length; i++) {
+            if(arrays[i].compareTo(min)<0){
+                min=arrays[i];
+            }
+        }
         return min;
     }
 
     public T max() {
         T max = arrays[0];
-        // FIXME: реализовать поиск наибольшего элемента используя метод compareTo интерфейса IComparable<T>
+        // FIXME: реализовать поиск наибольшего элемента используя метод compareTo интерфейса IComparable<T> +
         /*for(T item: arrays) {
             if(item > max) {
                 max = item;
             }
         }*/
+        for (int i = 0; i < arrays.length; i++) {
+            if(arrays[i].compareTo(max)>0){
+                max=arrays[i];
+            }
+        }
         return max;
+    }
+
+    public int compareTo(T o) {
+        return 0;
     }
 }
--- src/main/java/org/itstep/Tea.java
+++ src/main/java/org/itstep/Tea.java
@@ -1,13 +1,30 @@
 package org.itstep;
 
-// FIXME: реализовать интерфейс Comparable<Tea>. Метод compareTo() должен производить сравнение по цене
-public class Tea {
+// FIXME: реализовать интерфейс Comparable<Tea>. Метод compareTo() должен производить сравнение по цене +
+public class Tea implements Comparable<Tea> {
     private final String name;
     private final double price;
 
     public Tea(String name, double price) {
         this.name = name;
         this.price = price;
+    }
+
+    @Override
+    public int compareTo(Tea other) {
+        int res = 2;
+        if (this.price > other.price) {
+            res = 1;
+        } else {
+            if (this.price == other.price) {
+                res = 0;
+            } else {
+                if (this.price < other.price) {
+                    res = -1;
+                }
+            }
+        }
+        return res;
     }
 
     @Override
